5daede9e616ad6c6f19699ff3053749af4806d74
MatheusMikio <matheusmikio15@gmail.com>
2024-11-13 13:11:09 -0300

    Cabeçalho

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Index</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body>
@@ -30,6 +30,18 @@
                 </a>
             </li>
         </ul>
+    </div>
+    <div class="main--content">
+        <div class="header--wrapper">
+            <div class="header--title">
+                <span>Dashboard</span>
+                <h2>Sistema de Controle de Finanças</h2>
+            </div>
+            <div class="search">
+                <i class="fa-solid fa-search"></i>
+                <input type="text" placeholder="Pesquisar">
+            </div>
+        </div>
     </div>
 </body>
 </html>
