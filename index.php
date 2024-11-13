5a1c7de248d398635d98d891ffa506179042bd4e
MatheusMikio <matheusmikio15@gmail.com>
2024-11-12 22:28:42 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt=-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+</head>
+<body>
+    <div class="sidebar">
+        <div class="logo"></div>
+        <ul class="menu">
+            <li class="active">
+                <a href="index.php" >
+                    <i class="fas fa-tachometer-alt"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+            <li>
+                <a href="criar_despesa.php">
+                    <i class="fas fa-tachometer-alt"></i>
+                    <span>Criar despesa</span>
+                </a>
+            </li>
+            <li>
+                <a href="visualizar.php">
+                    <i class="fas fa-tachometer-alt"></i>
+                    <span>Visualizar meses</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</body>
+</html>
